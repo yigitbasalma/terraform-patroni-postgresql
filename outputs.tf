7bae32254525075ea49d6cc7ee5983882c33515c
Yiğit Can Başalma <yigit.basalma@warewave.tech>
2022-12-04 15:18:31 +0300

    Network LB address added for both ro and rw connections.

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "haproxy_private_ip_addresses" {
   value = module.haproxy.private_ip_addresses
 }
 
-output "extra_storages" {
-  value = module.postgres.extra_storages
+output "ro_lb_addr" {
+  value = "${module.haproxy.network_lb}:5001"
+}
+
+output "rw_lb_addr" {
+  value = "${module.haproxy.network_lb}:5000"
 }
